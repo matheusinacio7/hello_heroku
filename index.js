03c5f38141fa3d41b1c2e7c47ae66449a4b34988
heyset <c.inaciomatheus@gmail.com>
2021-11-12 12:25:46 -0300

    Adiciona mensagem secreta

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import helmet from 'helmet';
 
+const { SECRET_MESSAGE, PORT } = process.env;
+
 const app = express();
 
 app.use( helmet());
 
+app.get('/env', (_req, res) => {
+  res.status(200).json({ message: SECRET_MESSAGE });
+});
+
 app.get('/', (_req, res) => {
   res.status(200).json({ message: 'It\'s alive!' });
 });
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}.`);
